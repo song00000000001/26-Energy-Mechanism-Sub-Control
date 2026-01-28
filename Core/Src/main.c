b8770679b3ac705077107b02b94fa42284edec46
song <3554404410@qq.com>
2026-01-28 13:59:13 +0800

    都能正常输出24个序列了。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -52,102 +52,42 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-//30 + Num * 3 * 8 + 30
-#define WS2312_LED_NUM 1
-#define test_num_len (30 + WS2312_LED_NUM * 3 * 8 + 30)
-
 #define WS2312_0bit 29
 #define WS2312_1bit 50
 
-#define WS2812_delay 15
-
-#define arm_tim1 &htim3
-#define arm_channel_1 TIM_CHANNEL_1
-#define arm_channel_2 TIM_CHANNEL_3
-#define arm_channel_3 TIM_CHANNEL_4
-#define arm_tim2 &htim4
-#define arm_channel_4 TIM_CHANNEL_1
-#define arm_channel_5 TIM_CHANNEL_2
-#define arm_channel_6 TIM_CHANNEL_3
-
-uint16_t tim_pwm_dma_buff[test_num_len] = {0};//PWM DMA数据缓存
-uint8_t Pixel_Buff[WS2312_LED_NUM * 3] = {0};//RGB数据缓存
-
-void Set_Pixel_Color(uint8_t* buff,uint32_t index,uint8_t R,uint8_t G,uint8_t B)
+// DMA 完成回调函数
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
 {
-	buff[(index)*3] = G;
-	buff[(index)*3 + 1] = R;
-	buff[(index)*3 + 2] = B;
+    // 判定是哪个定时器触发的
+    if (htim->Instance == TIM3) {
+        // 传输完成后立即停止 DMA
+        // 停止顺序：先停通道，如果有必要可以手动把 CCR 清零
+        HAL_TIM_PWM_Stop_DMA(htim, TIM_CHANNEL_1);
+        HAL_TIM_PWM_Stop_DMA(htim, TIM_CHANNEL_3);
+        HAL_TIM_PWM_Stop_DMA(htim,TIM_CHANNEL_4);
+        // 强制清零 CCR，防止停止瞬间引脚保持高电平
+        __HAL_TIM_SET_COMPARE(htim, TIM_CHANNEL_1, 0);
+        __HAL_TIM_SET_COMPARE(htim, TIM_CHANNEL_3, 0);
+		__HAL_TIM_SET_COMPARE(htim, TIM_CHANNEL_4, 0);
+    }
 }
-
-void Buff_translate(uint8_t* buff,uint16_t* dma_buff) //颜色数组转换为码元数组
-{
-	for(uint32_t i = 0;i < (WS2312_LED_NUM*3);i++)
-	{
-		for(uint8_t k = 0;k < 8;k++)
-		{
-			if ( (buff[i] >> k) & 1)dma_buff[30 + (i * 8) + k] = WS2312_1bit;
-            else dma_buff[30 + (i * 8) + k] = WS2312_0bit;
-		}
-	}
-}
-/*灯臂
-主灯臂有5根灯条,其中1和5并联,2和4并联,3单独一根
-分别命名为主臂_外侧,主臂_中侧,主臂_内侧
-次灯臂有左右两条；各有2根灯条，而且直接并联；命名为次臂_左，次臂_右。
+//测试发现,dma搬数据的长度和输入的值无关,总是会搬16个数据,多余的数据是固定的不知道啥值,所以给16个测试一下
+/*
+static uint16_t test_buff[3][16] = {{WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,
+                                    WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit,WS2312_0bit},
+                                    {WS2312_1bit,WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_1bit,WS2312_0bit,
+                                     WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_1bit},
+                                    {WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,
+                                     WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit,WS2312_1bit}};
 */
-typedef enum 
-{
-    main_arm_outside = 1,
-    main_arm_middle = 2,
-    main_arm_inside = 3,
-    sub_arm_left = 4,
-    sub_arm_right = 5
-}ligntarm_name_enum;
-
-void armshow_red(uint8_t* buff,uint16_t* dma_buff,uint8_t num)
-{
-    for(uint8_t i=5;i<45;i++)
-    {	
-        Set_Pixel_Color(buff, i, 255, 0, 0);
-    }
-    Buff_translate(buff, dma_buff);
-    switch (num)
-    {
-    case main_arm_outside:
-        HAL_TIM_PWM_Start_DMA(arm_tim1, arm_channel_1, (uint32_t *)dma_buff, test_num_len);
-        break;
-    case main_arm_middle:
-        HAL_TIM_PWM_Start_DMA(arm_tim1, arm_channel_2, (uint32_t *)dma_buff, test_num_len);
-        break;
-    case main_arm_inside:
-        HAL_TIM_PWM_Start_DMA(arm_tim1, arm_channel_3, (uint32_t *)dma_buff, test_num_len);
-        break;
-    case sub_arm_left:
-        HAL_TIM_PWM_Start_DMA(arm_tim2, arm_channel_4, (uint32_t *)dma_buff, test_num_len);
-        break;
-    case sub_arm_right:
-        HAL_TIM_PWM_Start_DMA(arm_tim2, arm_channel_5, (uint32_t *)dma_buff, test_num_len);
-        break;
-    default:
-        break;
-    }
-    HAL_Delay(WS2812_delay);
-}
-
-void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)	//DMA回调函数
-{
-	/*HAL_TIM_PWM_Stop_DMA(htim,TIM_CHANNEL_1);
-	HAL_TIM_PWM_Stop_DMA(htim,TIM_CHANNEL_3);
-	HAL_TIM_PWM_Stop_DMA(htim,TIM_CHANNEL_4);*/
-}
-
+static uint16_t test_buff[3][27] = {{5,10,15,20,25 ,35,40,45,50,55 ,60,65,70,75,80 ,80,80,50,50,50 ,30,30,30,20,0 ,0,0}, 
+									{80,75,70,65,60 ,55,50,45,40,35 ,30,25,20,15,10 ,80,50,80,50,80 ,40,60,10,20,0 ,0,0},            
+                                    {60,29,29,29,29 ,29,29,29,29,29 ,29,44,29,10,21 ,80,50,60,40,30 ,50,60,40,20,0 ,0,0}};           
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-//uint32_t test_buff[10] = {0,0,WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_0bit,WS2312_1bit,WS2312_1bit,0,0};
-//uint16_t test_buff[2] = {WS2312_0bit,WS2312_1bit};
+
 /* USER CODE END 0 */
 
 /**
@@ -190,32 +130,19 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-
-    //测试pwm dma数据
-    /*for(uint8_t i=5;i<45;i++)
-    {	
-        Set_Pixel_Color(Pixel_Buff, i, 255, 0, 0);
-    }
-    Buff_translate(Pixel_Buff, tim_pwm_dma_buff);
-    HAL_Delay(WS2812_delay);*/
-	 
-    //HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t*)tim_pwm_dma_buff, test_num_len);//PC6
-    //HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_3, (uint32_t*)tim_pwm_dma_buff, test_num_len);//PC8
-    //HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_4, (uint32_t*)tim_pwm_dma_buff, test_num_len);//PC9
+    //HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t*)test_buff, 10);//PC6
+	//HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_3, (uint32_t*)test_buff, 10);//PC8
+    //HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_4, (uint32_t*)test_buff, 10);//PC9
     //HAL_TIM_PWM_Start_DMA(&htim4, TIM_CHANNEL_1, (uint32_t*)tim_pwm_dma_buff, test_num_len);//PB6
     //HAL_TIM_PWM_Start_DMA(&htim4, TIM_CHANNEL_2, (uint32_t*)tim_pwm_dma_buff, test_num_len);//PB7
     //HAL_TIM_PWM_Start_DMA(&htim4, TIM_CHANNEL_3, (uint32_t*)tim_pwm_dma_buff, test_num_len);//PB8
-
-    uint16_t test_buff[10] = {0,0,WS2312_0bit,WS2312_1bit,WS2312_0bit,WS2312_0bit,WS2312_1bit,WS2312_1bit,0,0};
-    HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t*)test_buff, 10);//PC6
-	HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_3, (uint32_t*)test_buff, 10);//PC8
-    HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_4, (uint32_t*)test_buff, 10);//PC9
     while (1)
     {
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
-        HAL_Delay(1000);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
-        HAL_Delay(1000);
+		HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t*)(test_buff[0]),27);//PC6
+		HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_3, (uint32_t*)(test_buff[1]), 27);//PC8
+        HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_4, (uint32_t*)(test_buff[2]), 27);//PC9
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+        HAL_Delay(200);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -177,7 +177,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     hdma_tim3_ch1_trig.Init.MemInc = DMA_MINC_ENABLE;
     hdma_tim3_ch1_trig.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_tim3_ch1_trig.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-    hdma_tim3_ch1_trig.Init.Mode = DMA_CIRCULAR;
+    hdma_tim3_ch1_trig.Init.Mode = DMA_NORMAL;
     hdma_tim3_ch1_trig.Init.Priority = DMA_PRIORITY_HIGH;
     if (HAL_DMA_Init(&hdma_tim3_ch1_trig) != HAL_OK)
     {
@@ -196,7 +196,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     hdma_tim3_ch3.Init.MemInc = DMA_MINC_ENABLE;
     hdma_tim3_ch3.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_tim3_ch3.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-    hdma_tim3_ch3.Init.Mode = DMA_CIRCULAR;
+    hdma_tim3_ch3.Init.Mode = DMA_NORMAL;
     hdma_tim3_ch3.Init.Priority = DMA_PRIORITY_HIGH;
     if (HAL_DMA_Init(&hdma_tim3_ch3) != HAL_OK)
     {
@@ -212,7 +212,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     hdma_tim3_ch4_up.Init.MemInc = DMA_MINC_ENABLE;
     hdma_tim3_ch4_up.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
     hdma_tim3_ch4_up.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-    hdma_tim3_ch4_up.Init.Mode = DMA_CIRCULAR;
+    hdma_tim3_ch4_up.Init.Mode = DMA_NORMAL;
     hdma_tim3_ch4_up.Init.Priority = DMA_PRIORITY_HIGH;
     if (HAL_DMA_Init(&hdma_tim3_ch4_up) != HAL_OK)
     {
